54d9678d9f8d5d9d641d40c2fc0b73126a41d6ed
beefybroccoli <tedkimemail@yahoo.com>
2021-10-08 03:39:10 -0700

    no comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,6 +20,7 @@ export const fetchFail = (input_error) => {
 };
 
 //FETCH_SMURFS - performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+//Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retrieve all smurfs from the api. Save the result of to our state and show an error if one is made.
 export const fetchSmurfs = () => (dispatch) => {
   //dispatch fetchStart
   //axios API call to API_URL
@@ -27,14 +28,21 @@ export const fetchSmurfs = () => (dispatch) => {
   //...failure - set error
 }; //end fetchSmurfs
 
+/**
+ * ADD_SMURFS - allows us to add new smurf (including the name, nickname, position, summary)
+ * @returns
+ */
 export const addSmurfs = () => (dispatch) => {
   //axios API call to API_URL
   //...success flag
   //...failure flag
 };
 
+/**
+ * allows us to set the value of the error message slice of state.
+ */
 export const setError = () => {};
-
+/*
 //ADD_SMURFS - allows us to add new smurf (including the name, nickname, position, summary)
 
 //SET_ERROR - allows us to set the value of the error message slice of state.
@@ -43,13 +51,13 @@ export const setError = () => {};
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
-
+*/
 /*
 ### Complete actions/index.js
- Add in the action creators and action constants needed to add a smurf to state and fetch smurfs from the server. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as nessisary.**
-  * [ ] Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive all smurfs from the api. Save the result of to our state and show an error if one is made.
-  * [ ] Add a standard action that allows us to add new smurf (including the name, nickname, position, summary).
-  * [ ] Add a standard action that allows us to set the value of the error message slice of state.
+//  Add in the action creators and action constants needed to add a smurf to state and fetch smurfs from the server. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as nessisary.**
+//   * [ ] Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retrieve all smurfs from the api. Save the result of to our state and show an error if one is made.
+//   * [ ] Add a standard action that allows us to add new smurf (including the name, nickname, position, summary).
+//   * [ ] Add a standard action that allows us to set the value of the error message slice of state.
   
   ### Complete App.js
   Connect component to the fetchSmurfs action.

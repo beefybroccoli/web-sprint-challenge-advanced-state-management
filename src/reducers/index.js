d87ff2bbbd19b1843d6be33ec19f33277b21c304
beefybroccoli <tedkimemail@yahoo.com>
2021-10-08 03:35:41 -0700

    no comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,12 +14,31 @@ Adds the following state values into the initialState:
 */
 export const initialState = {
   arrayOfSmurfs: [],
-  loading: true,
+  isLoading: false,
   errorString: "",
+  isFetching: false,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    // FETCH_START,
+    case FETCH_START:
+      return { ...state, isFetching: true };
+    // FETCH_SUCCESS,
+    case FETCH_SUCCESS:
+      return { ...state, isFetching: false };
+    // FETCH_FAIL,
+    case FETCH_FAIL:
+      return { ...state, isFetching: false };
+    // ADD_SMURFS,
+    case ADD_SMURFS:
+      return {
+        ...state,
+        arrayOfSmurfs: [...state.arrayOfSmurfs, action.payload],
+      };
+    // SET_ERROR,
+    case SET_ERROR:
+      return { ...state, errorString: action.payload };
     default:
       return state;
   }
@@ -32,29 +51,29 @@ export default reducer;
 //  - an array of smurfs
 //  - a boolean indicating if the app is loading
 //  - a string indicating a possible error message
-*/
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
 
+//2. Add in the arguments needed to complete a standard reducer function.
+//3. Add in a reducer case to accommodate the start of a smurf fetch.
+//4. Add in a reducer case to accommodate the successful smurf api fetch.
+//5. Add in a reducer cases to accommodate the failed smurf api fetch.
+//6. Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+//7. Add in a reducer case that adds in a value to the error message.
+*/
 /*
 #### Complete reducers/index.js
-  Add in the needed state and reducer cases to hold and modify smurf error messages, loading status and smurf data. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as nessisary.**
+  Add in the needed state and reducer cases too hold and modify smurf error messages, loading status and smurf data. **If at all possible, add in action cases one at a time, instead of all at once. Test your state connects and reducer cases as nessisary.**
 
-  * [ ] Adds the following state values into the initialState:
+  * [x] Adds the following state values into the initialState:
       - an array of smurfs
       - a boolean indicating if the app is loading
       - a string indicating a possible error message
 
   * [ ] Add in the arguments needed to complete a standard reducer function.
-  * [ ] Add in a reducer case to accommodate the start of a smurf fetch.
-  * [ ] Add in a reducer case to accommodate the successful smurf api fetch.
-  * [ ] Add in a reducer cases to accommodate an error.
-  * [ ] Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, description and an internally generated id) into your smurf list.
-  * [ ] Add in a reducer case that adds in a value to the error message.
+  * [x] Add in a reducer case to accommodate the start of a smurf fetch.
+  * [x] Add in a reducer case to accommodate the successful smurf api fetch.
+  * [x] Add in a reducer cases to accommodate an error.
+  * [x] Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, description and an internally generated id) into your smurf list.
+  * [x] Add in a reducer case that adds in a value to the error message.
   
   * Connect your application to reducer through redux with the thunk and logger middleware packages attached.
 */
